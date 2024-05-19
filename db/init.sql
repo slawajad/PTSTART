f4fe2caf5b7db2cc0bd74064c9246c47478d6a15
slawajad <slawa.jad@yandex.ru>
2024-05-20 02:27:02 +0300

    first commit

--- db/init.sql
+++ db/init.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS phone_numbers(
     phone_number VARCHAR(50) NOT NULL
 );
 
-INSERT INTO phone_numbers( phone_number) VALUES('88001234567');
-INSERT INTO phone_numbers( phone_number) VALUES('77007775555');
-INSERT INTO phone_numbers( phone_number) VALUES('86001230003');
+INSERT INTO phone_numbers( phone_number) VALUES('8 (800) 123-45-67');
+INSERT INTO phone_numbers( phone_number) VALUES('+7 (700) 777-55-55');
+INSERT INTO phone_numbers( phone_number) VALUES('8 (600) 123-00-03');
 
 INSERT INTO email_addresses(email) VALUES('info@example.com');
 INSERT INTO email_addresses(email) VALUES('contact@site.org');
